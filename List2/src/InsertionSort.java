a83cd15bfc66b3811bab6015842e22b439735f09
sqoshi <piotrpopis98@icloud.com>
2019-03-24 22:24:10 +0100

    Reupload

--- List2/src/InsertionSort.java
+++ List2/src/InsertionSort.java
@@ -5,7 +5,7 @@ public class InsertionSort {
     public InsertionSort(int[] arr) {
         double startTime = System.nanoTime();
         for (int i = 1; i < arr.length; ++i) {
-            int key = arr[i];K
+            int key = arr[i];
             int j = i - 1;
             while (j >= 0 && arr[j] > key) {
                 arr[j + 1] = arr[j];
