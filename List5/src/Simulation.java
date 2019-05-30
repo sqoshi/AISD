802737a5cf34319c5e53cfa55a575e9811c9aa1f
sqoshi <piotrpopis98@icloud.com>
2019-05-30 19:12:03 +0200

    ASL":dasdasd

--- List5/src/Simulation.java
+++ List5/src/Simulation.java
@@ -1,23 +1,21 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.Arrays;
 
 public class Simulation {
 
-    public static void S1(int n) throws IOException {
+    public static void S1(int n)  throws FileNotFoundException, IOException  {
 
 
-
-
-        int size=16;
+        int size = 16;
         int[][] breadthsgrouped;
         int[][] flows;
         double[][] times;
         double[][] OveralAverage;
         Graph graph;
-
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("output.txt"));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("output.txt"));
+        File file = new File("/home/piotr/IdeaProjects/AISD/List5/output.txt");
+StringBuilder str = new StringBuilder();
         for (int k = 1; k <= size; ++k) {
 
 
@@ -40,18 +38,36 @@ public class Simulation {
             double BreadthPatsAverageFork = Arrays.stream(breadthsgrouped[k]).average().getAsDouble();
             double FlowAverageFork = Arrays.stream(flows[k]).average().getAsDouble();
             double TimeAverageFork = Arrays.stream(times[k]).average().getAsDouble();
-            //OveralAverage[k][0] = BreadthPatsAverageFork;
-            //OveralAverage[k][1] = FlowAverageFork;
-            //OveralAverage[k][2] = TimeAverageFork;
-            PrintWriter writer = new PrintWriter("the-file-name.txt", "UTF-8");
-            writer.append((k )+ " " + BreadthPatsAverageFork + " " + FlowAverageFork + " " + TimeAverageFork);
-            writer.close();
+            OveralAverage[k][0] = BreadthPatsAverageFork;
+            OveralAverage[k][1] = FlowAverageFork;
+            OveralAverage[k][2] = TimeAverageFork;
+            //. writer.append((k )+ " " + BreadthPatsAverageFork + " " + FlowAverageFork + " " + TimeAverageFork);
+               str.append(k).append(" ").append(BreadthPatsAverageFork).append(" ").append(FlowAverageFork)
+                       .append(" ").append(TimeAverageFork).append("/");
+            System.out.println(str.toString());
+            System.out.println();
+                if(file.exists()) {
+
+
+                    String line = bufferedReader.readLine();
+                    System.out.println(line);
+                    while(line != null) {
+                        line = bufferedReader.readLine();
+                        if(line.equals("")) {
+                            bufferedWriter.write(str.toString());
+                            bufferedWriter.append(str.toString());
+                            System.out.println("a");
+                        }
+                        System.out.println(line);
+                    }
+                }
+            }
 
         }
-
+        //outputWriter.close();
 
 
     }
 
 
-}
+
