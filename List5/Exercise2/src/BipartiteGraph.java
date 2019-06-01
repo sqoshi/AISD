96138da6a8b6c392b2bc888d3c039a721691c68a
sqoshi <piotrpopis98@icloud.com>
2019-06-01 17:51:54 +0200

    generating biparte graph using all v from V2

--- List5/Exercise2/src/BipartiteGraph.java
+++ List5/Exercise2/src/BipartiteGraph.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Random;
 
@@ -13,7 +14,8 @@ public class BipartiteGraph {
         Random Random = new Random();
         size = (int) Math.pow(2, k + 1) + 2;
         adjList = new LinkedList[size];
-//BUILDING FLOW NETWORK ON ADJLIST
+
+        //BUILDING FLOW NETWORK ON ADJLIST
         for (int i = 0; i < size; i++) {
             adjList[i] = new LinkedList<>();
         }
@@ -27,7 +29,7 @@ public class BipartiteGraph {
             ArrayList<Integer> numbers = new ArrayList<Integer>();
             ArrayList<Integer> NUMBERS = new ArrayList<Integer>();
 
-            while (adjList[i].size() != 3) {
+            while (adjList[i].size() != m) {
                 randomNum = (int) (Random.nextInt((int) (Math.pow(2, k))) + Math.pow(2, k) + 1);
                 while (!NUMBERS.contains(randomNum)) {
                     NUMBERS.add(randomNum);
@@ -42,39 +44,63 @@ public class BipartiteGraph {
                 }
             }
         }
+    }
+    public void BFS(int s)
+    {
+        boolean visited[] = new boolean[size];
 
-        cap = new int[(int) (Math.pow(2, k + 1) + 2)][(int) (Math.pow(2, k + 1) + 2)];
-        // 2 zbiory po 2^k + sink i source
-        Random generator = new Random();
-        int random;
+        LinkedList<Integer> queue = new LinkedList<Integer>();
 
-        for (int j = 1; j < Math.pow(2, k) + 1; j++)
-            cap[0][j] = 1;
+        visited[s]=true;
+        queue.add(s);
 
-        for (int j = (int) (Math.pow(2, k) + 1); j < Math.pow(2, k + 1) + 1; j++)
-            cap[j][(int) (Math.pow(2, k + 1) + 1)] = 1;
+        while (queue.size() != 0)
+        {
+            s = queue.poll();
+            System.out.print(s+" ");
 
-        for (int j = 1; j < Math.pow(2, k) + 1; j++) {
-            random = (int) (generator.nextInt((int) (Math.pow(2, k))) + Math.pow(2, k) + 1);
-
-            for (int a = 0; a < m; a++) {
-                while (cap[j][random] != 0)
-                    random = (int) (generator.nextInt((int) (Math.pow(2, k))) + Math.pow(2, k) + 1);
-                cap[j][random] = 1;
+            Iterator<Integer> i = adjList[s].listIterator();
+            while (i.hasNext())
+            {
+                int n = i.next();
+                if (!visited[n])
+                {
+                    visited[n] = true;
+                    queue.add(n);
+                }
             }
         }
-
     }
+////      cap = new int[(int) (Math.pow(2, k + 1) + 2)][(int) (Math.pow(2, k + 1) + 2)];
+    //    // 2 zbiory po 2^k + sink i source
+    //    Random generator = new Random();
+    //    int random;
+
+    //    for (int j = 1; j < Math.pow(2, k) + 1; j++)
+    //        cap[0][j] = 1;
+
+    //    for (int j = (int) (Math.pow(2, k) + 1); j < Math.pow(2, k + 1) + 1; j++)
+    //        cap[j][(int) (Math.pow(2, k + 1) + 1)] = 1;
+
+    //    for (int j = 1; j < Math.pow(2, k) + 1; j++) {
+    //        random = (int) (generator.nextInt((int) (Math.pow(2, k))) + Math.pow(2, k) + 1);
+
+    //        for (int a = 0; a < m; a++) {
+    //            while (cap[j][random] != 0)
+    //                random = (int) (generator.nextInt((int) (Math.pow(2, k))) + Math.pow(2, k) + 1);
+    //            cap[j][random] = 1;
+    //        }
+    //    }
+
+    //}
 
     public static void main(String[] args) {
-        BipartiteGraph bipartiteGraph = new BipartiteGraph(3, 3);
+        BipartiteGraph bipartiteGraph = new BipartiteGraph(3, 2);
         printGraph(bipartiteGraph);
+        bipartiteGraph.BFS(8);
+
     }
 
-    static void addEdge(BipartiteGraph graph, int src, int dest) {
-        graph.adjList[src].add(dest);
-        graph.adjList[dest].add(src);
-    }
 
     public static void printer(int[][] matrix) {
 
--- List5/Exercise2/src/EdmondKarp.java
+++ List5/Exercise2/src/EdmondKarp.java
@@ -1,3 +1,5 @@
+import sun.awt.image.ImageWatched;
+
 import java.util.LinkedList;
 
 public class EdmondKarp {
@@ -10,20 +12,17 @@ public class EdmondKarp {
 
     boolean bfs(int rGraph[][], int s, int t, int parent[])
     {
-        // Create a visited array and mark all vertices as not
-        // visited
+
         boolean visited[] = new boolean[V];
         for(int i=0; i<V; ++i)
             visited[i]=false;
 
-        // Create a queue, enqueue source vertex and mark
-        // source vertex as visited
         LinkedList<Integer> queue = new LinkedList<Integer>();
         queue.add(s);
         visited[s] = true;
         parent[s]=-1;
 
-        // Standard BFS Loop
+        //bfs loop
         while (queue.size()!=0)
         {
             int u = queue.poll();
@@ -39,42 +38,55 @@ public class EdmondKarp {
             }
         }
 
-        // If we reached sink in BFS starting from source, then
-        // return true, else false
+        return (visited[t] == true);
+    }
+    boolean bfsonlist(LinkedList[] radj, int s, int t, int parent[])
+    {
+
+        boolean visited[] = new boolean[V];
+        for(int i=0; i<V; ++i)
+            visited[i]=false;
+
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+        queue.add(s);
+        visited[s] = true;
+        parent[s]=-1;
+
+        //bfs loop
+        while (queue.size()!=0)
+        {
+            int u = queue.poll();
+
+            for (int v=0; v<V; v++)
+            {
+                if (visited[v]==false && radj[u].contains(v))
+                {
+                    queue.add(v);
+                    parent[v] = u;
+                    visited[v] = true;
+                }
+            }
+        }
+
         return (visited[t] == true);
     }
 
-    // Returns tne maximum flow from s to t in the given graph
     int fordFulkerson(int graph[][], int s, int t)
     {
         int u, v;
 
-        // Create a residual graph and fill the residual graph
-        // with given capacities in the original graph as
-        // residual capacities in residual graph
-
-        // Residual graph where rGraph[i][j] indicates
-        // residual capacity of edge from i to j (if there
-        // is an edge. If rGraph[i][j] is 0, then there is
-        // not)
         int rGraph[][] = new int[V][V];
 
         for (u = 0; u < V; u++)
             for (v = 0; v < V; v++)
                 rGraph[u][v] = graph[u][v];
 
-        // This array is filled by BFS and to store path
         int parent[] = new int[V];
 
         int max_flow = 0;  // There is no flow initially
 
-        // Augment the flow while tere is path from source
-        // to sink
         while (bfs(rGraph, s, t, parent))
         {
-            // Find minimum residual capacity of the edhes
-            // along the path filled by BFS. Or we can say
-            // find the maximum flow through the path found.
             int path_flow = Integer.MAX_VALUE;
             for (v=t; v!=s; v=parent[v])
             {
@@ -82,8 +94,6 @@ public class EdmondKarp {
                 path_flow = Math.min(path_flow, rGraph[u][v]);
             }
 
-            // update residual capacities of the edges and
-            // reverse edges along the path
             for (v=t; v != s; v=parent[v])
             {
                 u = parent[v];
@@ -91,11 +101,9 @@ public class EdmondKarp {
                 rGraph[v][u] += path_flow;
             }
 
-            // Add path flow to overall flow
             max_flow += path_flow;
         }
 
-        // Return the overall flow
         return max_flow;
     }
 }
